e986624b9e1ce77b5d130bdb5ac1194c63ba9a95
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-10-27 09:39:20 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import User from "./components/User";
+import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [displayUsers, setDisplayUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=24", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.results)
+        setDisplayUsers(data.results)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <ButtonGroup
+        style={{
+          position: "absolute",
+          right: "0",
+        }}
+        disableElevation
+        variant="contained"
+        aria-label="Disabled elevation buttons"
+      >
+        <Button
+          onClick={() =>
+            setDisplayUsers(users.filter((user) => user.gender === "male"))
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Males
+        </Button>
+        <Button
+          onClick={() =>
+            setDisplayUsers(users.filter((user) => user.gender === "female"))
+          }
+          color="secondary"
+        >
+          Females
+        </Button>
+        <Button color="success" onClick={() => setDisplayUsers(users)}>
+          All
+        </Button>
+      </ButtonGroup>
+      <div className="App">
+        {displayUsers &&
+          displayUsers.map((user) => {
+            return <User user={user} />;
+          })}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function MediaCard({user}) {
+  return (
+    <Card  sx={{ maxWidth: 345, margin: "2% 2% 2% 2%" }} key={user.id.value}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={user.picture.medium}
+        alt={user.image_alt}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {user.name.title}. {user.name.first} {user.name.last}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user.gender}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user.email}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user.phone}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user.location.street.number}, {user.location.street.name},{" "}
+          {user.location.city}, {user.location.state}, {user.location.country},{" "}
+          {user.location.postcode}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {user.phone}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Contact</Button>
+      </CardActions>
+    </Card>
+  );
+}
